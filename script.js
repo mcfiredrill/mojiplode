c73e9f698bcd80b0ebb41dfa56075b953da6aaeb
Glitch (glitch-hello-website) <none>
2024-05-23 21:36:24 +0000

    ⛺️💑 Checkpoint ./index.html:2861572/2922 ./script.js:2861572/890

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 
 console.log('hey');
+const HOTDOG_URL = "wss://hotdog-lounge.herokuapp.com/socket";
+
+let socket = new Socket(HOTDOG_URL, {
+
+  logger: function logger(/*kind, msg, data*/) {
+    //console.log(kind + ": " + msg, data);
+  }
+});
+
+socket.connect();
+
+socket.onError(e => {
+  console.error('ERROR', e);
+});
+socket.onClose(e => {
+  console.log('CLOSE', e);
+})
+let nick = "mysterious_not_a_bot";
+let channel;
+
+channel = socket.channel("rooms:lobby", {});
+
+channel.join().receive("ignore", function () {
+  console.log("auth error");
+}).receive("ok", function () {
+  console.log("join ok");
+  channel.push("authorize", { user: nick, timestamp: Date.now() });
+}).receive("timeout", function () {
+  console.log("Connection interruption");
+});
+
+channel.on("authorized", msg => {
+  console.log('HI ' + msg.user + ' you are now authorized');
+});
