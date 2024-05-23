ead4ee1c19d7b800172a377c03468f2b680109b9
Glitch (glitch-hello-website) <none>
2024-05-23 22:33:46 +0000

    🐂👙 Checkpoint ./script.js:2861572/6181 ./index.html:2861572/44

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-console.log('hey');
+console.log("hey");
 const HOTDOG_URL = "wss://hotdog-lounge.herokuapp.com/socket";
 
 const urlPrefix = "https://datafruits.fm/assets/images/emojis/";
@@ -15,67 +14,81 @@ PIXI.utils.sayHello(type);
 
 //Create a Pixi Application
 let app = new PIXI.Application({
-  	autoResize: true,
-  resolution: devicePixelRatio 
-                               });
-
-//Add the canvas that Pixi automatically created for you to the HTML document
-window.addEventListener('load', () => {
-  document.body.appendChild(app.view);
+  view: document.getElementById("pixi"),
+  autoResize: true,
+  resolution: devicePixelRatio,
+  backgroundAlpha: 0,
 });
 
+app.stop();
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+window.addEventListener("load", () => {
+  document.body.appendChild(app.view);
+  app.start();
+});
 
 let socket = new Socket(HOTDOG_URL, {
-
   logger: function logger(/*kind, msg, data*/) {
     //console.log(kind + ": " + msg, data);
-  }
+  },
 });
 
 socket.connect();
 
-socket.onError(e => {
-  console.error('ERROR', e);
+socket.onError((e) => {
+  console.error("ERROR", e);
 });
-socket.onClose(e => {
-  console.log('CLOSE', e);
-})
+socket.onClose((e) => {
+  console.log("CLOSE", e);
+});
 let nick = "mysterious_not_a_bot";
 let channel;
 
 channel = socket.channel("rooms:lobby", {});
 
-channel.join().receive("ignore", function () {
-  console.log("auth error");
-}).receive("ok", function () {
-  console.log("join ok");
-  channel.push("authorize", { user: nick, timestamp: Date.now() });
-}).receive("timeout", function () {
-  console.log("Connection interruption");
+channel
+  .join()
+  .receive("ignore", function () {
+    console.log("auth error");
+  })
+  .receive("ok", function () {
+    console.log("join ok");
+    channel.push("authorize", { user: nick, timestamp: Date.now() });
+  })
+  .receive("timeout", function () {
+    console.log("Connection interruption");
+  });
+
+channel.on("authorized", (msg) => {
+  console.log("HI " + msg.user + " you are now authorized");
 });
 
-channel.on("authorized", msg => {
-  console.log('HI ' + msg.user + ' you are now authorized');
-});
-
-channel.on("new:msg", msg => {
+channel.on("new:msg", (msg) => {
   console.log(msg.body);
   const regex = /:(.*?):/g;
   let emojis = msg.body.match(regex).map((s) => s.slice(1, -1));
   console.log(emojis);
-  emojis.forEach(emoji => {
+  emojis.forEach((emoji) => {
     let url = `${urlPrefix}${emoji}.png`;
-    //let emojiSprite = PIXI.Sprite.from(url);
-    let emojiSprite = new Image();
-    emojiSprite.src = url;
-    document.body.appendChild(emojiSprite);
+    let emojiSprite = PIXI.Sprite.from(url);
+    //let emojiSprite = new Image();
+    //emojiSprite.src = url;
+    //document.body.appendChild(emojiSprite);
     emojiSprites.push(emojiSprite);
+    app.stage.addChild(emojiSprite);
   });
 });
 
 // do the animations
+let count = 0.0;
 app.ticker.add((delta) => {
-  emojiSprites.forEach(sprite => {
-    
+  emojiSprites.forEach((sprite) => {
+    sprite.x += Math.sin(count);
+    sprite.y += Math.cos(count);
+    sprite.scale.x += Math.sin(count) * 0.01;
+    sprite.scale.y += Math.sin(count) * 0.01;
+    sprite.rotation += Math.sin(count) * 0.01;
   });
+  count += 0.02 * delta;
 });
