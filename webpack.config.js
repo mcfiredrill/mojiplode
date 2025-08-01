d5512012a009009fab85cb973f4a9ccb7b6ec76f
Glitch (glitch-hello-website) <none>
2025-07-31 18:14:55 -0700

    setup build stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: __dirname,
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
-    port: 4200,
-    open: true,
-  },
-  mode: 'development',
+    mode: isProduction ? 'production' : 'development',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      port: 4200,
+      open: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'style.css', to: 'style.css' },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+  };
 };
